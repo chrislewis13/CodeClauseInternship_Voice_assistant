cd753be15b1dcaec1f75066ee5995ef89ba13e7f
chrislewis13 <143401065+chrislewis13@users.noreply.github.com>
2023-08-28 19:18:02 +0530

    Update Voice_assistant.py

--- Voice_assistant.py
+++ Voice_assistant.py
@@ -38,18 +38,20 @@ def play_HeyPython():
     instruction = input_instruction()
     print(instruction)
     if "play" in instruction:
-        song = instruction.replace('Play', "").strip()  # Remove leading/trailing spaces
+        song = instruction.replace('Play', "").strip()  
         talk("Playing " + song)
-        if len(song) > 100:  # Truncate the song if it's too long
+        if len(song) > 100: 
             song = song[:100]
         pywhatkit.playonyt(song)
 
     elif 'time' in instruction:
         time = datetime.datetime.now().strftime('%I:%M%p')
+        print(time)
         talk('current time'+ time)
 
     elif 'date' in instruction:
         date = datetime.datetime.now().strftime('%d /%m /%Y')
+        print(date)
         talk("Today's Date" + date)
 
 
@@ -60,14 +62,25 @@ def play_HeyPython():
         talk('I am Python, What can I do for you?')
 
     elif 'who is' in instruction:
-        human = instruction.replace('who is', "")
-        info = wikipedia.summary(human, 1)
-        print(info)
-        talk(info)
-
-
-
+        human = instruction.replace('who is', "").strip()
+        
+        if human:
+            try:
+                page = wikipedia.page(human)
+                info = page.summary
+                print(info)
+                talk(info)
+            except wikipedia.exceptions.PageError:
+                talk("I couldn't find information about that person.")
+            except wikipedia.exceptions.DisambiguationError as e:
+                first_option = e.options[0]
+                page = wikipedia.page(first_option)
+                info = page.summary
+                print(info)
+                talk(info)
+        else:
+            talk("Please specify a person's name after 'who is'.")
     else:
         talk("Couldn't hear you")
-
+    
 play_HeyPython()
